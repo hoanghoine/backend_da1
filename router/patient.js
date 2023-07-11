a7cb1a696f487bdea8193ca73af6a69ef659a4a3
Hoanghoine <dangtienhoang37@gmail.com>
2023-07-11 13:29:55 +0700

    nu -> nữ

--- router/patient.js
+++ router/patient.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {userController} from '../controller/index.js'
+import { userController } from '../controller/index.js'
 import middlewareVerifyToken from "../controller/middlewareController.js";
 import { ExpressValidator, body } from "express-validator";
 
@@ -15,23 +15,23 @@ router.post(
 router.get("/logout", userController.logout);
 
 router.post("/register",
-    body("name").isLength({ min: 3}),
-    body("dob").isDate({ format: 'YYYY-MM-DD'}),
-    body("sex").isIn(['nam', 'nu']),
+    body("name").isLength({ min: 3 }),
+    body("dob").isDate({ format: 'YYYY-MM-DD' }),
+    body("sex").isIn(['nam', 'nữ']),
     body("phoneNumber").isMobilePhone('vi-VN'),
-    body("BHYT").isNumeric().isLength({min: 10, max: 10}),
-    body("username").isEmail(),  
-userController.register);
+    body("BHYT").isNumeric().isLength({ min: 10, max: 10 }),
+    body("username").isEmail(),
+    userController.register);
 
 // router.get("/get-all-user",middlewareVerifyToken.verifyAdminAuth,userController.getAllUser); // lay tat ca thong tin user hien thi o trang thong tin user
 
-router.get("/:id",middlewareVerifyToken.verifyGetUserById,userController.getDetailUser); // lay tat ca thong tin user hien thi o trang thong tin user
+router.get("/:id", middlewareVerifyToken.verifyGetUserById, userController.getDetailUser); // lay tat ca thong tin user hien thi o trang thong tin user
 // Route quên mật khẩu
-router.post("/forgot-password", )
+router.post("/forgot-password",)
 
-router.post("/refresh-token",userController.reqRefreshToken)
+router.post("/refresh-token", userController.reqRefreshToken)
 // Route cập nhật thông tin tài khoản
-router.patch("/:id",middlewareVerifyToken.verifyGetUserById, userController.updateProfile);
+router.patch("/:id", middlewareVerifyToken.verifyGetUserById, userController.updateProfile);
 //delete for admin
 // router.delete("/delete-user/:id",middlewareVerifyToken.verifyAdminAuth,userController.deleteUser)
 
