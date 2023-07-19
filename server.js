9a292a6204efeb575a485573c94c4ceb3a21e054
Hoanghoine <dangtienhoang37@gmail.com>
2023-07-19 21:42:11 +0700

    cors

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ import bodyParser from 'body-parser'
 
 dotenv.config()
 const app = express()
-app.use(cors())
+app.use(cors(
+    {
+        origin: 'http://localhost:3000',
+        credentials: true
+    }
+))
 app.use(express.json())// đảm bảo rằng dữ liệu được gửi đến từ yêu cầu HTTP với kiểu Content-Type là application/json sẽ được phân tích và chuyển đổi thành JavaScript object
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
